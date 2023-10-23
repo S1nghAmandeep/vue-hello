d67b7e8ceec442220261b10930d64a14a03aceee
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-23 18:09:18 +0200

    added-key-bg-red

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ createApp({
             message: 'Hello World',
             img:'https://picsum.photos/seed/picsum/600/300',
             ans: 'Answer here',
+            bgRed: 'bgRed',
             desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, officiis beatae expedita assumenda at facilis sapiente quod ipsum dolorem dolore illum? Facere, ad esse eligendi quos quae ipsa labore consectetur.'
         }
     }
